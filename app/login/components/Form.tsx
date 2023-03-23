31529549a14ebbfdd6f05e77976c5156e06d0e78
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-23 12:00:46 +0100

    amended form login

--- app/login/components/Form.tsx
+++ app/login/components/Form.tsx
@@ -1,10 +1,15 @@
+'use client'; 
+
 import React from 'react'
 
 const Form = () => {
   return (
     <>
-    <div>Form</div>
-    <input type="text" placeholder='username'/>
+    <div onSubmit={(e) => e.preventDefault}>Form</div>
+    <form action="">
+        <input type="text" placeholder='username'/>
+        <button>Submit</button>
+    </form>
     </>
   )
 }
