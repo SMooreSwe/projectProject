3195df77e7819bf78d02ef390f90ea9c70939895
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-23 10:51:12 +0100

    first commit

---
+++ app/login/components/Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+    <div>Form</div>
+    <input type="text" placeholder='username'/>
+    </>
+  )
+}
+
+export default Form
---
+++ app/user/[user]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const user = () => {
+  return (
+    <div>User</div>
+  )
+}
+
+export default user 
