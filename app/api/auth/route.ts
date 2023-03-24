7c9d621ac19f7f536d173a12e6f0e14bb23ead6b
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-24 13:38:19 +0100

    deployment working

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,15 +1,10 @@
 import { signUpUser } from "./sign-up";
 
-// export async function GET(request: Request) {
-//   return new Response("Hello, Next.js!");
-// }
-
 export async function POST(request: Request) {
   const body = await request.json();
   const { username, email } = body;
   const data = await signUpUser(username, email);
 
-  console.log("----------POST function ---------");
   console.log(data);
   return new Response(JSON.stringify(data));
 }
