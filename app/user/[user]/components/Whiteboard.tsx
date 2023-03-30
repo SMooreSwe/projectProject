c695bee8c834130e7d382f05efc310acd15a6f62
luis barri <luisbarriw@gmail.com>
2023-03-30 14:32:13 +0200

    images for widgets v.21

--- app/user/[user]/components/Whiteboard.tsx
+++ app/user/[user]/components/Whiteboard.tsx
@@ -5,7 +5,7 @@ import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 import { PostIt } from "../WhiteboardComponents/PostIt";
 import { Textblock } from "../WhiteboardComponents/Text";
-import { WBImage } from "../WhiteboardComponents/WBImage";
+//import { WBImage } from "../WhiteboardComponents/WBImage";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -16,7 +16,13 @@ const Whiteboard = (props: {
 }) => {
   return (
     <ResponsiveGridLayout
-      layouts={{ lg: props.layouts, md: props.layouts, sm: props.layouts, xs: props.layouts, xxs: props.layouts }}
+      layouts={{
+        lg: props.layouts,
+        md: props.layouts,
+        sm: props.layouts,
+        xs: props.layouts,
+        xxs: props.layouts,
+      }}
       className="layout"
       compactType={null}
       preventCollision={false}
@@ -28,10 +34,8 @@ const Whiteboard = (props: {
       breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
       cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
     >
-      <PostIt key={'test'} data-grid={props.layouts[0]} />
-      <Textblock key={'text'} data-grid={props.layouts[1]}/>
-      <WBImage key={'image'} data-grid={props.layouts[2]}/>
-      
+      <PostIt key={"test"} data-grid={props.layouts[0]} />
+      <Textblock key={"text"} data-grid={props.layouts[1]} />
     </ResponsiveGridLayout>
   );
 };
