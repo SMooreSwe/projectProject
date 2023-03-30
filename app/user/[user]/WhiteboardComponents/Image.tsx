f4d8565a99c6536eaa6422f8e8df4a8ffdeca66f
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-30 12:03:10 +0200

    fixed void issues

--- app/user/[user]/WhiteboardComponents/Image.tsx
+++ app/user/[user]/WhiteboardComponents/Image.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+// import React from 'react'
 
-//eslint-disable-next-line react/display-name
-export const Image = React.forwardRef<HTMLDivElement>(({style, className, onMouseDown, onMouseUp, onTouchEnd, children, ...props}: any, ref) => {
-  const imgUrl = () => {
-    //return firestore image url for input
-    return 'hello' 
-  }
+// //eslint-disable-next-line react/display-name
+// export const Image = React.forwardRef<HTMLDivElement>(({style, className, onMouseDown, onMouseUp, onTouchEnd, children, ...props}: any, ref) => {
+//   const imgUrl = () => {
+//     //return firestore image url for input
+//     return 'hello' 
+//   }
 
-    return (
-    <img alt='' src={imgUrl()} style={style} className={className} ref={ref} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onTouchEnd={onTouchEnd}>
-     {children}
-    </img>
-  );
-})
+//     return (
+//     <img alt='' src={imgUrl()} style={style} className={className} ref={ref} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onTouchEnd={onTouchEnd}>
+//      {children}
+//     </img>
+//   );
+// })
