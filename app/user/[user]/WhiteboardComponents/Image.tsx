ce85ce2f4fd66612cefae5d980975c3d5a5fbbc7
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-03-30 12:01:31 +0200

    fixed void issues

--- app/user/[user]/WhiteboardComponents/Image.tsx
+++ app/user/[user]/WhiteboardComponents/Image.tsx
@@ -3,9 +3,10 @@ import React from 'react'
 //eslint-disable-next-line react/display-name
 export const Image = React.forwardRef<HTMLDivElement>(({style, className, onMouseDown, onMouseUp, onTouchEnd, children, ...props}: any, ref) => {
   const imgUrl = () => {
-    //return firestore image url for input 
+    //return firestore image url for input
+    return 'hello' 
   }
-  
+
     return (
     <img alt='' src={imgUrl()} style={style} className={className} ref={ref} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onTouchEnd={onTouchEnd}>
      {children}
