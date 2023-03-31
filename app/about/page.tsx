aff66d3057ae783ba33994fe4eb03a8ff3d5f90e
Britta <brittan.villegas@gmail.com>
2023-03-31 16:14:52 +0200

    fixed about page styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,11 @@ const About = () => {
   return (
     <main className='main__about-page'>
       <h1>About [project Project]</h1>
+
+      <section className='section section___project-who'>
+      <h2 className='section__title'>Who We Are</h2>
+      <p>We are four people with completely different nationalities, career backgrounds, and skillsets who are all motivated by the same thing: creating practical and accessible software.</p>
+      </section>
       
       <section className='section section__project-what'>
       <h2 className='section__title'>What It Is</h2>
@@ -27,42 +32,22 @@ const About = () => {
       <section className='section section__project-how'>
       <h2 className='section__title'>How We Did It</h2>
       <p>From the beginning, we found balance between thorough planning and remaining agile. We planned our must-haves, like which components, collections, documents, queries, and CRUD operations our application absolutely needed. How we implemented these things and how it would ultimatley look remained agile. If an approach or library did not work for us within a few hours, we scrapped it and searched for something more effective and time efficient.</p>
-      <Image 
+      <div className="about-page__images-container">
+      <Image
         src="/first-sketch.jpg"
         alt="[project Project]'s very first UI sketch"
-        width={800}
-        height={500}
+        width={600}
+        height={375}
       />
       <Image 
+      className='section__project-img'
       src="/component-mapping.jpg"
       alt="first draft of our components and application architecture"
-      width={800}
-      height={500}
+      width={600}
+      height={375}
       />
-      <Image 
-      src="/mvp.png"
-      alt="sketch of our original mvp"
-      width={800}
-      height={500}
-      />
-      <Image 
-      src="/query-logic.png"
-      alt="mapping of our query logic"
-      width={800}
-      height={500}
-      />
-      <Image 
-      src="/data-model.png"
-      alt="sketch of our data model"
-      width={800}
-      height={500}
-      />
+      </div>
       <p>View the [project Project] Github repo <a href="https://github.com/SMooreSwe/projectProject" target="_blank">here</a>.</p>
-      </section>
-
-      <section className='section section___project-who'>
-      <h2 className='section__title'>Who We Are</h2>
-      <p>We are four people with completely different nationalities, career backgrounds, and skillsets who are all motivated by the same thing: creating practical and accessible software.</p>
       </section>
     </main>
   )
