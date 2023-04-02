7126024a8970858917ef28074f95b69abe1a8a0f
luis barri <luisbarriw@gmail.com>
2023-04-02 16:27:14 +0200

    merge

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,23 @@ export default function Home() {
       <main className="login__main-container">
         <h1 className="login__title">[project Project]</h1>
         <p className="login__subtitle">Login with your email and password.</p>
-<<<<<<< HEAD
         <Form />
-        <p>
-          Dont have an account? Click{" "}
-          <Link className="login__message link" href="/signup">
-            here
-          </Link>{" "}
-          to sign up.
-        </p>
-        <p>
-          Learn more about us{" "}
-          <Link href="/about" className="link">
-            here
-          </Link>
-          .
-        </p>
-=======
-        <Form /> 
         <section className="login__container-more-info-section">
-          <p className="more-info__signup">Dont have an account? Click <Link className="login__message link" href="/signup" >here</Link> to sign up.</p>
-          <p>Learn more about us <Link href="/about" className="link">here</Link>.</p>
+          <p className="more-info__signup">
+            Dont have an account? Click{" "}
+            <Link className="login__message link" href="/signup">
+              here
+            </Link>{" "}
+            to sign up.
+          </p>
+          <p>
+            Learn more about us{" "}
+            <Link href="/about" className="link">
+              here
+            </Link>
+            .
+          </p>
         </section>
->>>>>>> 1dee9b81a87d622257557dfc02267858563c0d77
       </main>
     </>
   );
