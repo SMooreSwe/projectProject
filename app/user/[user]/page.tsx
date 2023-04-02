7419de4c4995dd131000011ec2c6aec39c0440c5
SMooreSwe <118370783+SMooreSwe@users.noreply.github.com>
2023-04-02 17:04:32 +0200

    fix project list

--- app/user/[user]/page.tsx
+++ app/user/[user]/page.tsx
@@ -49,12 +49,10 @@ const User = () => {
       querySnapshot.forEach((doc) => {
         data.push(doc.data());
       });
-      if (data.length < 0) {
         setProjectList(data);
         setCurrentProject(data[0].projectid);
         setCurrentProjectName(data[0].name);
         setCurrentProjectMembers(data[0].users);
-      }
     });
   };
 
